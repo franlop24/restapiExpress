efe651a0a43cb612ac63b335d4ec46865d2f1c71
franlops <franlopbri@gmail.com>
2023-05-21 17:00:50 -0600

    Trabajando con rutas

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
  res.send('Hola mi server en Express');
 });
 
+app.get('/nueva-ruta', (req, res) => {
+ res.send('Hola soy un nuevo endpoint');
+});
+
+app.get('/products', (req, res) => {
+ res.json({
+  name: "Producto 1",
+  price: 1000
+ });
+});
+
 app.listen(port, () => {
   console.log('Mi port ' + port);
 });
