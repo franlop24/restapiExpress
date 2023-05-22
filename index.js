76736f8f8153d8ec315bbefde4fdb593e76113fe
franlops <franlopbri@gmail.com>
2023-05-21 19:36:54 -0600

    separacion de responsabilidades express.Router

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { faker }  = require('@faker-js/faker');
+const routerApi = require('./routes');
 
 const app = express();
 const port = 3000;
@@ -12,52 +12,7 @@ app.get('/nueva-ruta', (req, res) => {
  res.send('Hola soy un nuevo endpoint');
 });
 
-app.get('/products', (req, res) => {
-  const { size } = req.query;
-  const products = [];
-  const limit = size || 100;
-  for(let i = 0; i < limit; i++){
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.url()
-    })
-  }
- res.json(products);
-});
-
-app.get('/products/filter', (req, res) => {
-  res.send('Yo soy un filter');
-});
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id: id,
-    name: "Producto 1",
-    price: 1000
-   });
-})
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId
-  });
-});
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query
-  if(limit && offset){
-    res.json({
-      limit,
-      offset
-    });
-  } else {
-    res.send('No hay parámertos');
-  }
-});
+routerApi(app);
 
 app.listen(port, () => {
   console.log('Mi port ' + port);
---
+++ routes/products.router.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { faker } = require('@faker-js/faker');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const { size } = req.query;
+  const products = [];
+  const limit = size || 100;
+  for(let i = 0; i < limit; i++){
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.url(),
+    });
+  }
+ res.json(products);
+});
+
+router.get('/filter', (req, res) => {
+  res.send('Yo soy un filter');
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id: id,
+    name: "Producto 1",
+    price: 1000
+   });
+});
+
+module.exports = router;
