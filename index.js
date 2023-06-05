99be950d5f8a3bed9baa69ac6e2c5e9985352bae
franlops <franlopbri@gmail.com>
2023-06-04 19:19:43 -0600

    Middlewares cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
@@ -7,6 +8,19 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+// const whitelist = ['http://localhost:5500', 'https://myapp.co'];
+// const options = {
+//   origin: (origin, callback) => {
+//     if (whitelist.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('no permitido'));
+//     }
+//   }
+// }
+// app.use(cors);
+
 
 app.get('/', (req, res) => {
  res.send('Hola mi server en Express');
@@ -17,6 +31,7 @@ app.get('/nueva-ruta', (req, res) => {
 });
 
 routerApi(app);
+// app.use(cors);
 
 app.use(logErrors);
 app.use(boomErrorHandler);
